c2e5b7a8eee8a6beb9e78cbe5c047a8048607276
seongho00 <tjdwns000927@gmail.com>
2025-05-30 20:25:44 +0900

    달력 데이터 가져오기

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.TripTaleProjectApplication;
 import com.example.demo.service.ArticleService;
 import com.example.demo.service.KakaoOAuthService;
-import com.example.demo.vo.Article;
 import com.example.demo.vo.Rq;
 
 @Controller
@@ -31,7 +32,6 @@ public class UsrHomeController {
 	@RequestMapping("usr/home/main")
 	public String main(Model model) {
 
-
 		return "usr/home/main";
 	}
 
@@ -40,13 +40,41 @@ public class UsrHomeController {
 
 		return "usr/home/main";
 	}
-	
+
+	@RequestMapping("usr/home/test")
+	public String test(Model model) {
+		Calendar cal = Calendar.getInstance();
+		int year = 2025;
+		int month = 5;
+		System.out.printf("\t\t%d년 %d월\n", year, month);
+		System.out.printf("일\t월\t화\t수\t목\t금\t토\n");
+
+		cal.set(year, month - 1, 1);
+
+		int start = cal.get(Calendar.DAY_OF_WEEK);
+
+		for (int i = 1; i < start; i++) {
+			System.out.print("\t");
+		}
+
+		for (int i = 1; i <= cal.getActualMaximum(Calendar.DATE); i++) {
+			System.out.printf("%d\t", i);
+			if (start % 7 == 0) {
+				System.out.println();
+			}
+			start++;
+		}
+		
+
+		return "usr/home/test";
+	}
+
 	@RequestMapping("usr/test/kakaoMapTest")
 	public String kakaoMapTest(Model model) {
 
 		return "usr/test/kakaoMapTest";
 	}
-	
+
 	@RequestMapping("usr/test/GeoJSONTest")
 	public String GeoJSONTest(Model model) {
 
